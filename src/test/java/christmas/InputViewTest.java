87273d765c77b821e76f4e9ae5484e463dc6a245
dd <12202340@inha.edu>
2023-11-13 00:56:18 +0900

    test: inputDate에 대한 테스트 리팩토링

--- src/test/java/christmas/InputViewTest.java
+++ src/test/java/christmas/InputViewTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,20 +21,20 @@ public class InputViewTest {
     }
 
     @DisplayName("날짜 입력할 때, 숫자 이외의 다른 문자가 포함되어 있으면 에러 발생")
-    @Test
-    void validateTypeTest() {
-        String testDate = "hi";
-        System.setIn(new ByteArrayInputStream(testDate.getBytes()));
+    @ValueSource(strings = {"hi", " ", "-"})
+    @ParameterizedTest
+    void validateTypeTest(String inputDate) {
+        System.setIn(new ByteArrayInputStream(inputDate.getBytes()));
 
         assertThatThrownBy(() -> InputView.inputDate())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("날짜 입력할 때, 1 이상 31 이하의 범위가 아닌 숫자를 입력하면 에러 발생")
-    @Test
-    void validateRangeTest() {
-        String testDate = "-5";
-        System.setIn(new ByteArrayInputStream(testDate.getBytes()));
+    @ValueSource(strings = {"-5", "-35"})
+    @ParameterizedTest
+    void validateRangeTest(String inputDate) {
+        System.setIn(new ByteArrayInputStream(inputDate.getBytes()));
 
         assertThatThrownBy(() -> InputView.inputDate())
                 .isInstanceOf(IllegalArgumentException.class);
