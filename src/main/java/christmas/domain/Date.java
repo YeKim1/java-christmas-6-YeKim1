35006fc13c82a450b1139e60943d92c404d8dd0c
dd <12202340@inha.edu>
2023-11-13 01:50:19 +0900

    feat(Date.java): 날짜가 평일인지 주말인지 확인하는 기능 구현

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,6 +1,10 @@
 package christmas.domain;
 
+import java.util.List;
+
 public class Date {
+    private static final List<Integer> WEEKEND_FIRST_DAYS = List.of(1, 2);
+    private static final int DAYS = 7;
 
     private final int date;
 
@@ -10,5 +14,12 @@ public class Date {
 
     public static Date from(String inputDate) {
         return new Date(Integer.parseInt(inputDate));
+    }
+
+    public boolean isWeekend() {
+        if (WEEKEND_FIRST_DAYS.contains(date % DAYS)) {
+            return true;
+        }
+        return false;
     }
 }
