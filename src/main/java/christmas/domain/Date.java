bf04f5c285cd00ed8ed16a76cb56b1f3f4441ca5
dd <12202340@inha.edu>
2023-11-13 01:53:36 +0900

    feat(Date.java): 날짜가 특별 할인 날짜인지 확인하는 기능 구현

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Date {
     private static final List<Integer> WEEKEND_FIRST_DAYS = List.of(1, 2);
+    private static final List<Integer> SPECIAL_DISCOUNT_DAYS = List.of(3, 10, 17, 24, 25, 26, 31);
     private static final int DAYS = 7;
 
     private final int date;
@@ -17,9 +18,10 @@ public class Date {
     }
 
     public boolean isWeekend() {
-        if (WEEKEND_FIRST_DAYS.contains(date % DAYS)) {
-            return true;
-        }
-        return false;
+        return WEEKEND_FIRST_DAYS.contains(date % DAYS);
+    }
+
+    public boolean isSpecialDay() {
+        return SPECIAL_DISCOUNT_DAYS.contains(date);
     }
 }
