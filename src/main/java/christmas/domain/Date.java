63e9cb027900bce0c4d81d278b5c6cdd2d9fbf78
dd <12202340@inha.edu>
2023-11-12 23:10:01 +0900

    refactor: Date에 대한 유효성 검증을 InputView에서 하도록 수정

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,9 +1,6 @@
 package christmas.domain;
 
 public class Date {
-    private static final String DATE_ERROR_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    private static final int MIN = 1;
-    private static final int MAX = 31;
 
     private final int date;
 
@@ -12,28 +9,6 @@ public class Date {
     }
 
     public static Date from(String inputDate) {
-        validate(inputDate);
         return new Date(Integer.parseInt(inputDate));
-    }
-
-    private static void validate(String inputDate) {
-        validateType(inputDate);
-        validateRange(inputDate);
-    }
-
-    private static void validateType(String inputDate) {
-        try {
-            Integer.parseInt(inputDate);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(DATE_ERROR_MESSAGE);
-        }
-    }
-
-    private static void validateRange(String inputDate) {
-        int date = Integer.parseInt(inputDate);
-
-        if (date < MIN || date > MAX) {
-            throw new IllegalArgumentException(DATE_ERROR_MESSAGE);
-        }
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,8 +3,34 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String DATE_ERROR_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+
     public static String inputDate() {
         OutputView.printInputDateMessage();
-        return Console.readLine();
+        return dateValidate(Console.readLine());
+    }
+
+    private static String dateValidate(String inputDate) {
+        validateDateType(inputDate);
+        validateDateRange(inputDate);
+        return inputDate;
+    }
+
+    private static void validateDateType(String inputDate) {
+        try {
+            Integer.parseInt(inputDate);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(DATE_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateDateRange(String inputDate) {
+        int date = Integer.parseInt(inputDate);
+
+        if (date < MIN_DATE || date > MAX_DATE) {
+            throw new IllegalArgumentException(DATE_ERROR_MESSAGE);
+        }
     }
 }
