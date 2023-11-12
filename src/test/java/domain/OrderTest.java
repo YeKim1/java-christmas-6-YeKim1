67cc70ac3b26eb55e97a13dde42c6ca3f7f4f595
dd <12202340@inha.edu>
2023-11-13 01:38:17 +0900

    test(OrderTest.java): 총주문 금액을 계산하는 메소드 테스트 코드 작성

--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -5,8 +5,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderTest {
+
+    @DisplayName("총주문 금액을 계산하는 메소드 테스트")
+    @Test
+    void calculateTotalPriceTest() {
+        int answer = 73000;
+
+        Order order = Order.from("해산물파스타-2,제로콜라-1");
+
+        assertThat(answer).isEqualTo(order.calculateTotalPrice());
+    }
 
     @DisplayName("메뉴판에 없는 메뉴를 입력한 경우 예외 발생")
     @Test
