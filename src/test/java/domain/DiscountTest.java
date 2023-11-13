787d12da989f59baa8e4854cf866737d21e7dfc5
dd <12202340@inha.edu>
2023-11-13 20:56:03 +0900

    test(DiscountTest.java): 총 혜택 금액을 계산하는 기능 테스트

--- src/test/java/domain/DiscountTest.java
+++ src/test/java/domain/DiscountTest.java
@@ -10,12 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DiscountTest {
 
-    @DisplayName("해당하는 Event만 필드의 Map에 저장한다.")
+    @DisplayName("총혜택 금액 계산 테스트")
     @Test
-    void ofTest() {
-        Discount discount1 = Discount.of(VisitDate.from("5"), Order.from("티본스테이크-5"));
-        Discount discount2 = Discount.of(VisitDate.from("19"), Order.from("바비큐립-5"));
+    void getSumOfDiscount() {
+        Discount discount = Discount.of(VisitDate.from("1"), Order.from("티본스테이크-3,바비큐립-5,제로콜라-1"));
 
-        assertThat(discount1).usingRecursiveComparison().isEqualTo(discount2);
+        assertThat(discount.getSumOfDiscount()).isEqualTo(42184);
     }
 }
