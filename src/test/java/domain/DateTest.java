beef351c9c6444e9ff6cf1faa0de53738a12e015
dd <12202340@inha.edu>
2023-11-13 17:19:49 +0900

    test(DateTest.java): Date.java에 추가된 메서드 isLessThan, isContainedIn 테스트 코드 작성

--- src/test/java/domain/DateTest.java
+++ src/test/java/domain/DateTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import christmas.domain.Date;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +9,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DateTest {
 
-    @DisplayName("날짜가 평일인지 주말인지 확인하는 기능")
+    @DisplayName("date 값이 주말인지 계산하는 메서드 테스트")
     @Test
     void isWeekendTest() {
+        Date date = Date.from("22");
+
+        assertThat(true).isEqualTo(date.isWeekend());
+    }
+
+    @DisplayName("date 값이 주어진 날짜보다 작은지 검사하는 메서드 테스트")
+    @Test
+    void isLessThanTest() {
         Date date = Date.from("4");
 
-        assertThat(false).isEqualTo(date.isWeekend());
+        assertThat(false).isEqualTo(date.isLessThan(4));
+    }
+
+    @DisplayName("date 값이 주어진 List 안에 속하는지 검사하는 메서드 테스트")
+    @Test
+    void isContainedInTest() {
+        Date date = Date.from("5");
+
+        assertThat(true).isEqualTo(date.isContainedIn(List.of(1,2,3,4,5)));
     }
 }
