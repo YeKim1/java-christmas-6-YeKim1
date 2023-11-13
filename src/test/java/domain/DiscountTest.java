247d7415a79e1f0cd02cd9ff004fd6efd47093b7
dd <12202340@inha.edu>
2023-11-13 17:42:35 +0900

    feat(DiscountTest.java): Discount 생성자 테스트 코드 작성

---
+++ src/test/java/domain/DiscountTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import christmas.domain.Date;
+import christmas.domain.Discount;
+import christmas.domain.Order;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DiscountTest {
+
+    @DisplayName("해당하는 Event만 필드의 Map에 저장한다.")
+    @Test
+    void ofTest() {
+        Discount discount1 = Discount.of(Date.from("5"), Order.from("티본스테이크-5"));
+        Discount discount2 = Discount.of(Date.from("19"), Order.from("바비큐립-5"));
+
+        assertThat(discount1).usingRecursiveComparison().isEqualTo(discount2);
+    }
+}
