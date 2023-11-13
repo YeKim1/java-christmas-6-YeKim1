19f4122c63e5cb3350b30b902f84900017c65a37
dd <12202340@inha.edu>
2023-11-13 17:21:03 +0900

    feat(Event.java): BiPredicate 필드를 추가해서 할인 적용 여부를 검사하는 기능 구현

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -1,17 +1,25 @@
 package christmas.domain;
-;
+
+import java.util.List;
 import java.util.function.BiPredicate;
 
 public enum Event {
-    CHRISTMAS_DISCOUNT("크리스마스 디데이 할인"),
-    WEEKEND_DISCOUNT("주말 할인"),
-    WEEKDAY_DISCOUNT("평일 할인"),
-    SPECIAL_DISCOUNT("특별 할인"),
-    GIFTS_EVENT("증정 이벤트");
+    CHRISTMAS_DISCOUNT("크리스마스 디데이 할인", (date, order) -> date.isLessThan(26)),
+    WEEKEND_DISCOUNT("주말 할인", (date, order) -> date.isWeekend()),
+    WEEKDAY_DISCOUNT("평일 할인", (date, order) -> !date.isWeekend()),
+    SPECIAL_DISCOUNT("특별 할인", (date, order) ->
+            date.isContainedIn(List.of(3, 10, 17, 24, 25, 31))),
+    GIFTS_EVENT("증정 이벤트", (date, order) -> order.calculateTotalPrice() >= 120000);
 
-    String label;
+    private final String label;
+    private final BiPredicate<Date, Order> condition;
 
-    Event(String label) {
+    Event(String label, BiPredicate<Date, Order> condition) {
         this.label = label;
+        this.condition = condition;
+    }
+
+    public boolean isMeetCondition(Date date, Order order) {
+        return this.condition.test(date, order);
     }
 }
