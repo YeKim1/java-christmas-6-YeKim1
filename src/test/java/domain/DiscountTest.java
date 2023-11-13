e223452a3062cdac47e3574678719a47e9326304
dd <12202340@inha.edu>
2023-11-14 01:13:22 +0900

    test(DiscountTest.java): 증정 메뉴 반환 메서드 테스트 코드 작성

--- src/test/java/domain/DiscountTest.java
+++ src/test/java/domain/DiscountTest.java
@@ -3,6 +3,7 @@ package domain;
 import christmas.domain.VisitDate;
 import christmas.domain.Discount;
 import christmas.domain.Order;
+import java.util.Collections;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,23 @@ public class DiscountTest {
     void getSumOfDiscount() {
         Discount discount = Discount.of(VisitDate.from("1"), Order.from("티본스테이크-3,바비큐립-5,제로콜라-1"));
 
-        assertThat(discount.getSumOfDiscount()).isEqualTo(42184);
+        assertThat(discount.getSumOfDiscountPrice()).isEqualTo(42184);
+    }
+
+    @DisplayName("혜택이 없을 경우, 총혜택 금액은 0이어야 한다.")
+    @Test
+    void getSumofDiscountIfNonDiscount() {
+        Discount discount = Discount.of(VisitDate.from("2"), Order.from("타파스-1"));
+
+        assertThat(discount.getSumOfDiscountPrice()).isEqualTo(0);
+    }
+
+    @DisplayName("증정 메뉴가 없을 경우, Empty Map을 반환한다.")
+    @Test
+    void getGiftMenuOrEmptyTest() {
+        Discount discount = Discount.of(VisitDate.from("2"), Order.from("초코케이크-1"));
+
+        assertThat(discount.getGiftMenuOrEmpty()).isEqualTo(Collections.EMPTY_MAP);
+
     }
 }
